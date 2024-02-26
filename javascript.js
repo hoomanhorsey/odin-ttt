9720bdc11cd0e3f07cf36401f32ee39e82476d39
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2024-02-26 16:48:42 +1100

    working on css

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+// TODO
+// IF there is a winner, 
+// - then update screen to advise winner.
+// - prompt to start new game.
+
+// Prompt user: 
+// -  to input names?
+// - choose maker?
+
+
 document.addEventListener("DOMContentLoaded", function() {
 
 // Gameboard object IIFE
@@ -15,18 +25,16 @@ const Gameboard = (function() {
 
     // method to add piece to game board
     const addPiece = (coord1, coord2, piece) => {
-        console.log("Adding piece to Board", coord1, coord2, piece);       
-        board[coord1][coord2] = piece;
-        //displayBoard.display(Gameboard.getBoard(), "Called from addpiece");
-        }
+        //console.log("Adding piece to Board", coord1, coord2, piece);       
+        board[coord1][coord2] = piece;}
 
     // method to get game board
     const getBoard = () => board;
 
     // method to print game board to console
-    const printBoard = () => console.table(board);
+    // const printBoard = () => console.table(board);
 
-    return {addPiece, getBoard, printBoard};
+    return {addPiece, getBoard};
     })();
 
 
@@ -36,41 +44,12 @@ function player(playername) {
     const symbol = "";
     let marker = "";
 
-    const getName = name;
+    const getName = name; // Is this redundant
     const setMarker = (input) => marker = input;
     const getMarker = () => marker; 
 
-        return { getName, name, setMarker, getMarker}
+        return { getName, setMarker, getMarker}
     }
-
-// // Display Object IIFE
-// const displayBoard = (function() {
-   
-//     const display = (calledBoard, message) => {
-//     let board = calledBoard;
-
-//     console.log('displayboard has been called ' + message)
-
-//     let boardDisplay = document.querySelector('.boardDisplay');
-//     boardDisplay.textContent = ' ';
-
-//     for (let b = 0; b < 3; b++) {
-//         let row = document.createElement('div');
-//         row.setAttribute('class', 'row')        
-//         boardDisplay.appendChild(row);    
-
-//         for (let n = 0; n < 3; n++) {
-        
-//             let cell = document.createElement('p');
-//             cell.textContent = board[b][n];
-//             cell.setAttribute('class', 'cell')
-//             row.appendChild(cell);
-//             }}      
-//         }
-//     return { display } 
-
-//     })();
-
 
 // gameControl object  
 const gameControl = (function() {        
@@ -78,61 +57,29 @@ const gameControl = (function() {
       // checkTurn method (checks board to determine if play is possible)
     const checkBoardTurn = (activePlayerName, coords) => {
 
-        console.log('coords ' + coords)
+        // console.log('coords ' + coords)
         let board = Gameboard.getBoard();
-        console.table(board)
+        // console.table(board)
         let row = coords[0];
-        let column = coords[2];
+        let column = coords[2];      
         
-        
-             
-        if ((board[row][column] === 'X') ||  (board[row][column] === 'O'))
-        { 
-            alert('Position is already taken, please re-enter')
-            alert('need to change the function that calls this alert, to accomodate a different return objbect')
-            return;
-        } else {
-            console.log('turn okay')
-            return { row, column }
-        
-        } 
-        
-        
-        // if (board[row][column] === ' ') 
-        //         { 
-        //         console.log('turn okay')
-        //         return { row, column }
-        //         } 
-        //     else if  (board[row][column] != ' ' )  {
-        //      alert('Position is already taken, please re-enter')
-        //      alert('need to change the function that calls this alert, to accomodate a different return objbect')
-        //      return;
-        //     }
-        
-        // return { row, column }
-        
-        
-        // do {
-        //     if (board[row][column] === ' ') 
-        //         { 
-        //         console.log('turn okay')
-        //         break; 
-        //         } 
-        //     alert('Position is already taken, please re-enter')
-        //     break;
-        // } while (board[row][column] != ' ' );      
-        
-        // return { row, column }
+        do {
+            if (board[row][column] === ' ') {
+                return { row, column }
+            }
+            alert('Woops! That position is already taken. Please choose a different space');
+            return 'space taken';
+        } while ((board[row][column] === 'X') ||  (board[row][column] === 'O'));
     } 
 
     // change Active Player method
     const changeActivePlayer = (activePlayer) => {
-        return activePlayer = activePlayer.name === playerOne.name ? playerTwo : playerOne;
+        return activePlayer = activePlayer.getName === playerOne.getName ? playerTwo : playerOne;
     }
         
     // checkWin method
     const checkWin = () => {
-    console.log("Currently testing conditions - checkwin called")
+    // console.log("Currently testing conditions - checkwin called")
 
         board = Gameboard.getBoard();
         // const testboard = [['O', 'X', 'X'], ['X', 'O', 0], [0, 'X', 0]];
@@ -194,23 +141,19 @@ const gameControl = (function() {
         if (tie === 0) {
             return "Tie";
         }
-         
     } // end of CheckWin method
 
     // winnerMessage method
     const winnerMessage = (winner) => {
         if (winner === 'X') {
             alert('xwinner')
-              return ('winner is ' + winner)
-            
+              return ('winner is ' + winner) 
           } else if (winner === 'O') {
             alert('Owinner')
-            return ('winner is ' + winner)
-            
+            return ('winner is ' + winner)     
           } else if (winner === 'Tie') {
             alert('Tiewinner')
             return ('Game is tied!')
-            
           }
     }
  
@@ -235,70 +178,56 @@ const gameControl = (function() {
     }
 
     const getActivePlayer = function () {
-        return activePlayer.name;
+        return activePlayer.getName;
     };
     
-// Logic for game turns, check winnner, change player
-
+    //Initialize hard coded player names
     let {playerOne, playerTwo, winner, activePlayer }=  initialize();
   
-
-
     const playBoardRound = (coords) => {   
 
         // calls checkTurn to see if turn possible, returns turn co-ords
         let gameturn = checkBoardTurn(activePlayer.name, coords);
-        console.log('gameturn' + gameturn.row + gameturn.column)
-    
+        // console.log('gameturn' + gameturn.row + gameturn.column);
+        if (gameturn === 'space taken') {
+            return;
+        }
         // calls add piece to update board
         Gameboard.addPiece(gameturn.row, gameturn.column, activePlayer.getMarker())
     
         activePlayer = changeActivePlayer(activePlayer); 
-        console.log('activePlayer', activePlayer)                                    
+        // console.log('activePlayer', activePlayer)                                    
             };
-  
-
-   return { getActivePlayer, playBoardRound, checkWin, winnerMessage } 
-
+    return { getActivePlayer, playBoardRound, checkWin, winnerMessage } 
     })();
-
-
 
 const screenController  = (function () {
 
-    let boardDisplay = document.querySelector('.boardDisplay');
-
-    
-
+    let boardDisplay = document.querySelector('.boardDisplay'); 
 
     const clickHandlerCell = () => {
 
         let cells = document.querySelectorAll('.cell');
         cells.forEach((e) => {
             e.addEventListener('click', (e) => {
-            console.log(e.target)    
-            console.log(e.target.dataset.index)
+            // console.log(e.target)    
+            // console.log(e.target.dataset.index)
 
             gameControl.playBoardRound(e.target.dataset.index);
-            //displayBoard.display(Gameboard.getBoard(), "Called from clickHandlerCell");
-            // screenController.updateScreen('from ClickHandlerCell');
             updateScreen(' from clickHandlerCell 2nd time')
             setTimeout(function() {
                 winner = gameControl.checkWin();
-                console.log('checkwin from settimeout')
                 winMessage = gameControl.winnerMessage(winner);
                     if (winMessage !== undefined) {
                     alert(winMessage);
-                    }
-            }, 250);
-
-        } 
+                    }}, 250);
+            } 
         )})
     };
 
     const updateScreen = (message) => {  
      
-        console.log('updateScreen has been' + message)
+        // console.log('updateScreen has been' + message)
     
         // clear board
         boardDisplay.textContent = ' ';
@@ -311,7 +240,6 @@ const screenController  = (function () {
         turnID.textContent = "Player turn: " + gameControl.getActivePlayer();
         whoseTurn.textContent = ' ';    
         whoseTurn.appendChild(turnID);
-        //#TODO
     
         // render board
         for (let b = 0; b < 3; b++) {
@@ -330,15 +258,12 @@ const screenController  = (function () {
             clickHandlerCell();
             }   
 
-        
-
-
     return {updateScreen, clickHandlerCell}
 })();
 
 screenController.updateScreen(' from global');
 
-screenController.clickHandlerCell();
+//screenController.clickHandlerCell();
 
 
 
